2d15e61886884c290afd4745cfce6ca53efa12d7
Meli <bodzamelani@gmail.com>
2021-03-19 00:43:20 +0100

    implemented GreenParkingMeters & YellowParkingMeters

---
+++ GreenParkingMeters.js
@@ -0,0 +1,193 @@
+import { Marker } from 'react-native-maps'
+import React from 'react'
+
+export default function GreenParkingMeters() {
+  return (
+    <>
+      <Marker
+        coordinate={{ latitude: 46.256899, longitude: 20.156419 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.256916, longitude: 20.154902 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.25658, longitude: 20.157263 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.257654, longitude: 20.152677 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.256933, longitude: 20.149887 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.256197, longitude: 20.149839 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.255984, longitude: 20.150689 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.255365, longitude: 20.150171 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.254216, longitude: 20.152779 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.25574, longitude: 20.153981 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.256074, longitude: 20.153375 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.256597, longitude: 20.152474 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.258489, longitude: 20.151323 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.258699, longitude: 20.149716 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.258711, longitude: 20.148398 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.254803, longitude: 20.149006 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.254033, longitude: 20.150071 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.254176, longitude: 20.151707 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.253278, longitude: 20.149493 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.253653, longitude: 20.148045 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.25351, longitude: 20.146749 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.250293, longitude: 20.146917 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.249441, longitude: 20.147542 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.24977, longitude: 20.150899 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.250818, longitude: 20.151138 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.250798, longitude: 20.149819 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.248793, longitude: 20.151021 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.246448, longitude: 20.148567 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.246126, longitude: 20.145589 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.246814, longitude: 20.144173 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.246964, longitude: 20.147285 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.248357, longitude: 20.147064 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.248486, longitude: 20.144972 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.249024, longitude: 20.143923 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.247594, longitude: 20.146249 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.249907, longitude: 20.142758 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.249864, longitude: 20.143673 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.252175, longitude: 20.14392 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.25334, longitude: 20.145353 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.252306, longitude: 20.145948 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.253945, longitude: 20.143993 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.254928, longitude: 20.144515 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.255583, longitude: 20.143494 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.255984, longitude: 20.145378 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.255444, longitude: 20.146282 }}
+        image={require('./assets/zone-green.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.256561, longitude: 20.147118 }}
+        image={require('./assets/zone-green.png')}
+      />
+    </>
+  )
+}
---
+++ YellowParkingMeters.js
@@ -0,0 +1,258 @@
+import { Marker } from 'react-native-maps'
+import React from 'react'
+
+export default function YellowParkingMeters() {
+  return (
+    <>
+      <Marker
+        coordinate={{ latitude: 46.259405, longitude: 20.147244 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.260689, longitude: 20.149308 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.259129, longitude: 20.151099 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.260696, longitude: 20.149362 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.260484, longitude: 20.152911 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.25853, longitude: 20.153075 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.259327, longitude: 20.153566 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.259169, longitude: 20.156026 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.25806497477452,
+          longitude: 20.154496054259198,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.258903, longitude: 20.157742 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.258328, longitude: 20.15963 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.257724, longitude: 20.161164 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.257079, longitude: 20.162 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.256508, longitude: 20.159817 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.257232, longitude: 20.159366 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.258883351791056,
+          longitude: 20.160522775532016,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{ latitude: 46.258292, longitude: 20.163049 }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.258957760911805,
+          longitude: 20.161957340817548,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.25977372587244,
+          longitude: 20.16095687686874,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.26105542342778,
+          longitude: 20.156501163788906,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.26024504086845,
+          longitude: 20.15622489627723,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.25771428803652,
+          longitude: 20.145071789856193,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.256106014907736,
+          longitude: 20.141831874349787,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.254426441949484,
+          longitude: 20.142497010003922,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.25341379621717,
+          longitude: 20.140185044312293,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.25418353938121,
+          longitude: 20.140663499596567,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.25295944417565,
+          longitude: 20.141454545685882,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.25142487509607,
+          longitude: 20.14191958993312,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.250313877955804,
+          longitude: 20.140173471882438,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.24947736943328,
+          longitude: 20.14002595038711,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.248731734669825,
+          longitude: 20.139242745329902,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.24810294523969,
+          longitude: 20.141299999559873,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.247568746783635,
+          longitude: 20.139358080304756,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.24608128961904,
+          longitude: 20.14019250523118,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.246229185828255,
+          longitude: 20.141704863603024,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.24711211589547,
+          longitude: 20.142879671079864,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.2457802957651,
+          longitude: 20.14260608577704,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.244993801821494,
+          longitude: 20.141586846374935,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.244177616931,
+          longitude: 20.143319553389713,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.24349097910446,
+          longitude: 20.144615847707644,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.244943712248734,
+          longitude: 20.1450277108527126,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+      <Marker
+        coordinate={{
+          latitude: 46.24395046992013,
+          longitude: 20.14602713426968,
+        }}
+        image={require('./assets/zone-yellow.png')}
+      />
+    </>
+  )
+}
