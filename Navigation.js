54ac311553b9bfa3729874c7636cc9d0c27d8c93
Meli <bodzamelani@gmail.com>
2021-03-18 19:32:29 +0100

    implemented function navigation

---
+++ Navigation.js
@@ -0,0 +1,13 @@
+import { Linking } from "react-native";
+
+export default function navigation(latitude, longitude) {
+    const scheme = Platform.select({ ios: 'maps:0,0?q=', android: 'geo:0,0?q=' });
+    const latLng = `${latitude},${longitude}`;
+    const label = 'Custom Label';
+    const url = Platform.select({
+      ios: `${scheme}${label}@${latLng}`,
+      android: `${scheme}${latLng}(${label})`
+    });
+    
+   return Linking.openURL(url)
+}
