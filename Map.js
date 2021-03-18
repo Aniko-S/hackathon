44726f879c7b474c82613d01722a3855d7522ee9
Roland Németh <info@nemethroland.hu>
2021-03-18 15:40:50 +0100

    Reformat coordinates

--- Map.js
+++ Map.js
@@ -1,10 +1,8 @@
 import React from "react";
-import MapView, { Polygon } from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView, {Polygon} from "react-native-maps";
+import {Dimensions, StyleSheet, View} from "react-native";
 
 function Map() {
-    46.257223, 20.157388
-
     const greenCordinates = [
         {latitude: 46.256037, longitude: 20.157685},
         {latitude: 46.256596, longitude: 20.157353},
@@ -92,44 +90,44 @@ function Map() {
         {latitude: 46.245626, longitude: 20.150097},
         {latitude: 46.245589, longitude: 20.150279},
         {latitude: 46.245733, longitude: 20.150385},
-        {longitude: 20.1502079, latitude: 46.2457273},
-{longitude: 20.1505297, latitude: 46.2457532},
-{longitude: 20.1508516, latitude: 46.2458015},
-{longitude: 20.1517421, latitude: 46.2484094},
-{longitude: 20.1525468, latitude: 46.251021},
-{longitude: 20.1532334, latitude: 46.2522562},
-{longitude: 20.1542419, latitude: 46.2535471},
-{longitude: 20.1550949, latitude: 46.2546117},
-{longitude: 20.156731, latitude: 46.2554351},
-{longitude: 20.1577717, latitude: 46.2559321},
-{longitude: 20.1577127, latitude: 46.2560137},
-{longitude: 20.1602232, latitude: 46.2564774},
-{longitude: 20.1621866, latitude: 46.2570152},
-{longitude: 20.1638228, latitude: 46.2576161},
-{longitude: 20.1638013, latitude: 46.2577644},
-{longitude: 20.1633936, latitude: 46.2577088},
-{longitude: 20.1631254, latitude: 46.2582466},
-{longitude: 20.1630771, latitude: 46.2583282},
-{longitude: 20.1634312, latitude: 46.2584691},
-{longitude: 20.1630825, latitude: 46.2591516},
-{longitude: 20.162605, latitude: 46.2595225},
-{longitude: 20.1614892, latitude: 46.260153},
-{longitude: 20.1604056, latitude: 46.2606389},
-{longitude: 20.1589519, latitude: 46.2610172},
-{longitude: 20.15782, latitude: 46.2613176},
-{longitude: 20.1509857, latitude: 46.261529},
-{longitude: 20.1473808, latitude: 46.2609504},
-{longitude: 20.1428747, latitude: 46.2587251},
-{longitude: 20.1393557, latitude: 46.2547044},
-{longitude: 20.1384759, latitude: 46.2519743},
-{longitude: 20.1378107, latitude: 46.2492589},
-{longitude: 20.138154, latitude: 46.2472705},
-{longitude: 20.1392913, latitude: 46.2454602},
-{longitude: 20.1406217, latitude: 46.2444696},
-{longitude: 20.1432717, latitude: 46.2431452},
-{longitude: 20.1444948, latitude: 46.2425886},
-{longitude: 20.1457715, latitude: 46.2436237},
-{longitude: 20.1472145, latitude: 46.2453489},
+        {latitude: 46.2457273, longitude: 20.1502079},
+        {latitude: 46.2457532, longitude: 20.1505297},
+        {latitude: 46.2458015, longitude: 20.1508516},
+        {latitude: 46.2484094, longitude: 20.1517421},
+        {latitude: 46.251021, longitude: 20.1525468},
+        {latitude: 46.2522562, longitude: 20.1532334},
+        {latitude: 46.2535471, longitude: 20.1542419},
+        {latitude: 46.2546117, longitude: 20.1550949},
+        {latitude: 46.2554351, longitude: 20.156731},
+        {latitude: 46.2559321, longitude: 20.1577717},
+        {latitude: 46.2560137, longitude: 20.1577127},
+        {latitude: 46.2564774, longitude: 20.1602232},
+        {latitude: 46.2570152, longitude: 20.1621866},
+        {latitude: 46.2576161, longitude: 20.1638228},
+        {latitude: 46.2577644, longitude: 20.1638013},
+        {latitude: 46.2577088, longitude: 20.1633936},
+        {latitude: 46.2582466, longitude: 20.1631254},
+        {latitude: 46.2583282, longitude: 20.1630771},
+        {latitude: 46.2584691, longitude: 20.1634312},
+        {latitude: 46.2591516, longitude: 20.1630825},
+        {latitude: 46.2595225, longitude: 20.162605},
+        {latitude: 46.260153, longitude: 20.1614892},
+        {latitude: 46.2606389, longitude: 20.1604056},
+        {latitude: 46.2610172, longitude: 20.1589519},
+        {latitude: 46.2613176, longitude: 20.15782},
+        {latitude: 46.261529, longitude: 20.1509857},
+        {latitude: 46.2609504, longitude: 20.1473808},
+        {latitude: 46.2587251, longitude: 20.1428747},
+        {latitude: 46.2547044, longitude: 20.1393557},
+        {latitude: 46.2519743, longitude: 20.1384759},
+        {latitude: 46.2492589, longitude: 20.1378107},
+        {latitude: 46.2472705, longitude: 20.138154},
+        {latitude: 46.2454602, longitude: 20.1392913},
+        {latitude: 46.2444696, longitude: 20.1406217},
+        {latitude: 46.2431452, longitude: 20.1432717},
+        {latitude: 46.2425886, longitude: 20.1444948},
+        {latitude: 46.2436237, longitude: 20.1457715},
+        {latitude: 46.2453489, longitude: 20.1472145}
         // {latitude: 46.246074, longitude: 20.150657},
         // {latitude: 46.250290, longitude: 20.151940},
         // {latitude: 46.251844, longitude: 20.152584},
@@ -141,26 +139,26 @@ function Map() {
         // {latitude: 46.256035, longitude: 20.157639},
     ]
 
-  return (
-    <View style={styles.container}>
-        <MapView
-            style={styles.map}
-            initialRegion={{
-                latitude: 46.253,
-                longitude: 20.148,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }}
-        >
-            <Polygon
-              //key={polygon.id}
-              coordinates={yellowCordinates}
-              //holes={polygon.holes}
-              strokeColor="rgba(245, 229, 27, 1)"
-              fillColor="rgba(245, 229, 27, 0.4)"
-              strokeWidth={1}
-            />
-            {/* <Polygon
+    return (
+        <View style={styles.container}>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 46.253,
+                    longitude: 20.148,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            >
+                <Polygon
+                    //key={polygon.id}
+                    coordinates={yellowCordinates}
+                    //holes={polygon.holes}
+                    strokeColor="rgba(245, 229, 27, 1)"
+                    fillColor="rgba(245, 229, 27, 0.4)"
+                    strokeWidth={1}
+                />
+                {/* <Polygon
               //key={polygon.id}
               coordinates={greenCordinates}
               //holes={polygon.holes}
@@ -168,23 +166,23 @@ function Map() {
               fillColor="rgba(63, 195, 128, 0.2)"
               strokeWidth={1}
             /> */}
-        </MapView>
-          
-    </View>
-  );
+            </MapView>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    map: {
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+    },
 });
 
 export default Map;
